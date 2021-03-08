9988724378c9d2a330db3a95f3cc9446e3a3a068
unknown <fridolin@memory-path.com>
2021-03-08 12:30:02 +0100

    Added Unique Id

--- lib/src/MemoryPoint.dart
+++ lib/src/MemoryPoint.dart
@@ -3,22 +3,25 @@ import 'package:hive/hive.dart';
 part 'MemoryPoint.g.dart';
 
 @HiveType(typeId: 1)
-class MemoryPointDb extends HiveObject{
+class MemoryPointDb extends HiveObject {
   @HiveField(0)
-  String image;
-  @HiveField(5)
-  String name;
+  int id;
   @HiveField(1)
-  String question;
+  String image;
+  @HiveField(6)
+  String name;
   @HiveField(2)
-  String answer;
+  String question;
   @HiveField(3)
-  double lat;
+  String answer;
   @HiveField(4)
+  double lat;
+  @HiveField(5)
   double long;
 
   MemoryPointDb(
-      {this.image,
+      {this.id,
+      this.image,
       this.question,
       this.answer,
       this.lat,
