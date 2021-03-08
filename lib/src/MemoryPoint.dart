b8d5ed88d8e888f9f7e6acb1f1807c138585fc27
unknown <fridolin@memory-path.com>
2021-03-08 15:21:05 +0100

    Removed MemoryPointId

--- lib/src/MemoryPoint.dart
+++ lib/src/MemoryPoint.dart
@@ -5,26 +5,18 @@ part 'MemoryPoint.g.dart';
 @HiveType(typeId: 1)
 class MemoryPointDb extends HiveObject {
   @HiveField(0)
-  int id;
-  @HiveField(1)
   String image;
-  @HiveField(6)
-  String name;
-  @HiveField(2)
-  String question;
-  @HiveField(3)
-  String answer;
-  @HiveField(4)
-  double lat;
   @HiveField(5)
+  String name;
+  @HiveField(1)
+  String question;
+  @HiveField(2)
+  String answer;
+  @HiveField(3)
+  double lat;
+  @HiveField(6)
   double long;
 
   MemoryPointDb(
-      {this.id,
-      this.image,
-      this.question,
-      this.answer,
-      this.lat,
-      this.long,
-      this.name});
+      {this.image, this.question, this.answer, this.lat, this.long, this.name});
 }
